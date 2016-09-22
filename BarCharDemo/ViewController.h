727065c029025baf5f943c58087b7149e248e1fb
zhanglu <lu.zhang@tqmall.com>
2016-09-22 16:15:48 +0800

    Initial Commit

---
+++ BarCharDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BarCharDemo
+//
+//  Created by zhanglu on 9/22/16.
+//  Copyright © 2016 zhanglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
